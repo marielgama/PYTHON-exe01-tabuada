f50bbd1acae12b0ac408002aef33591ded6a58f4
Marci. Ariel Gama <148490113+marielgama@users.noreply.github.com>
2024-09-06 12:03:18 -0300

    Add files via upload

---
+++ aula05-FOR-RANGE-TABUADA.py
@@ -0,0 +1,7 @@
+numero = int(input('Digite um número inteiro entre 1 e 10: '))
+for x in range(1, 11):
+    resultado = numero * x
+    print(f'{numero} x {x} = {resultado}')
+
+print('.......................')
+
